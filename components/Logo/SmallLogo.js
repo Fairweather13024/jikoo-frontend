3a3eee40f00ffb051d8d4052bf70398e1b752ea9
Mark Eric Fairweather <59223278+Fairweather13024@users.noreply.github.com>
2022-03-25 03:27:20 +0000

    Create small logo component

---
+++ components/Logo/SmallLogo.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+const SmallLogo = () => {
+  return (
+  <div className='p-2'><Image src='/smallLogo.jpg' width={60} height={60}/></div>
+    
+  )
+}
+
+export default SmallLogo
